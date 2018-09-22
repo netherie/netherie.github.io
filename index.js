f0533dd6104f39ab405a6bf4c24462e5531280f3
netherie <37778253+netherie@users.noreply.github.com>
2018-09-23 01:38:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- var config = {
+var config = {
     type: Phaser.AUTO,
     width: 1600,
     height: 1200,
@@ -17,32 +17,31 @@
 
 
 
-   let cursors;
-   let map;
-   let layer;
-   let sprite;
-   let player;
-   let gold = 0;
-   let chest;
+let cursors;
+let map;
+let layer;
+let sprite;
+let player;
+let gold = 0;
+let chest;
+let skeleton;
 
-    let game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-    function preload ()
-	{
+
+function preload ()
+{
 	
-	this.load.image('tileset', "tilesets/tileset1.png");
-	this.load.tilemapTiledJSON('map', "../maps/map13.json");
-    this.load.spritesheet("archer", 
-        "sprites/archer.png",
-        { frameWidth: 64, frameHeight: 64 }
-    );
-	this.load.spritesheet('thief', 'sprites/thief.png', { frameWidth: 64, frameHeight: 64});
-	}
+this.load.image('tileset', "tilesets/tileset1.png");
+this.load.tilemapTiledJSON('map', "../maps/map13.json");
+this.load.spritesheet('thief', 'sprites/thief.png', { frameWidth: 64, frameHeight: 64});
+this.load.spritesheet('skeleton', 'sprites/skeleton.png', { frameWidth: 64, frameHeight: 64});
+}
 	
 
    
    
-   function create ()
+function create ()
 {
     const map = this.make.tilemap({ key: "map" });
 	
@@ -60,22 +59,31 @@
 	
 	const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
 	
-	player = this.physics.add.sprite(100, 450, 'thief');
+    player = this.physics.add
+    .sprite(spawnPoint.x, spawnPoint.y, 'thief')
+    .setOffset(0, 0)
+    .setSize(31, 60);
+
+    skeleton = this.physics.add.sprite(400, 300, 'skeleton');
+
+
+    
     
     player.setCollideWorldBounds(true);
 
 
+	
+
 	this.anims.create({
-        key: 'thief-walk-forward',
-        frames: this.anims.generateFrameNumbers('thief', { start: 104, end: 112 }),
+        key: 'thief-walk-back',
+        frames: this.anims.generateFrameNumbers('thief', { start: 130, end: 138 }),
         frameRate: 10,
         repeat: -1
     });
 
-	
-	this.anims.create({
-        key: 'thief-walk-back',
-        frames: this.anims.generateFrameNumbers('thief', { start: 130, end: 138 }),
+    this.anims.create({
+        key: 'thief-walk-forward',
+        frames: this.anims.generateFrameNumbers('thief', { start: 104, end: 112 }),
         frameRate: 10,
         repeat: -1
     });
@@ -93,49 +101,78 @@
         frameRate: 10,
         repeat: -1
     });
+
+  
+
+	this.anims.create({
+        key: 'skeleton-walk-back',
+        frames: this.anims.generateFrameNumbers('skeleton', { start: 130, end: 138 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'skeleton-walk-forward',
+        frames: this.anims.generateFrameNumbers('skeleton', { start: 104, end: 112 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+   this.anims.create({
+        key: 'skeleton-walk-left',
+        frames: this.anims.generateFrameNumbers('skeleton', { start: 117, end: 125 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'skeleton-walk-right',
+        frames: this.anims.generateFrameNumbers('skeleton', { start: 143, end: 151 }),
+        frameRate: 10,
+        repeat: -1
+    });
 	
     this.physics.add.collider(player, worldLayer);
     this.physics.add.collider(player, belowLayer);
 	
-	const camera = this.cameras.main;
+    const camera = this.cameras.main;
+    camera.setRoundPixels(true);
 	camera.startFollow(player);
-	camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    camera.setBounds(0, 0, 1600*2, 1600*2);
+    
+    this.physics.world.setBounds(0, 0, 1600, 1600);
 	
 	cursors = this.input.keyboard.createCursorKeys();
 
 }	
 	
 
-    function update ()
-    {
+function update ()
+{
+    const speed = 175;
+    const prevVelocity = player.body.velocity.clone();
 	
-	
-	const speed = 175;
-	const prevVelocity = player.body.velocity.clone();
-	
-	
-	player.body.setVelocity(0);
-	
+    
+    player.body.setVelocity(0);
 	
 	
 	if (cursors.left.isDown) {
-	player.body.setVelocityX(-speed);
-  } else if (cursors.right.isDown) {
-    player.body.setVelocityX(speed);
-  }
-
-
-  if (cursors.up.isDown) {
-    player.body.setVelocityY(-speed);
-  } else if (cursors.down.isDown) {
-    player.body.setVelocityY(speed);
-  }
-
-
-  player.body.velocity.normalize().scale(speed);
-  
-
-if (cursors.left.isDown) {
+        player.body.setVelocityX(-speed);
+    } else if (cursors.right.isDown) {
+        player.body.setVelocityX(speed);
+    }
+    
+    
+    if (cursors.up.isDown) {
+        player.body.setVelocityY(-speed);
+    } else if (cursors.down.isDown) {
+        player.body.setVelocityY(speed);
+    }
+    
+    player.body.velocity.normalize().scale(speed);
+    
+    
+    if (cursors.left.isDown) {
       player.anims.play("thief-walk-left", true);
     } else if (cursors.right.isDown) {
         player.anims.play("thief-walk-right", true);
@@ -146,5 +183,10 @@ if (cursors.left.isDown) {
     } else {
         player.anims.stop();
     }
+
+    //player = this.physics.add
+    //.sprite(spawnPoint.x, spawnPoint.y, 'thief')
+   // .setOffset(0, 0)
+   // .setSize(31, 60);
 }
 
